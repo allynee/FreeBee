c891d7ecc27a221f3cc322a7723a5324ce4d63a3
allynee <86574138+allynee@users.noreply.github.com>
2023-03-06 19:55:11 +0800

    Commenting out

--- backend/sql_app/models.py
+++ backend/sql_app/models.py
@@ -9,23 +9,23 @@ from database import Base
 
 
 # database table
-class User(Base):
-    __tablename__ = "users"
+# class User(Base):
+#     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False, index=True)
-    email = Column(String(100), nullable=False, unique=True, index=True)
-    address = Column(String(100), nullable=False)
-    hashed_password = Column(String(100), nullable=False)
-    is_active = Column(Boolean, default=True)
-    items = relationship("Item", back_populates="owner")
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String(100), nullable=False, index=True)
+#     email = Column(String(100), nullable=False, unique=True, index=True)
+#     address = Column(String(100), nullable=False)
+#     hashed_password = Column(String(100), nullable=False)
+#     is_active = Column(Boolean, default=True)
+#     items = relationship("Item", back_populates="owner")
 
-# database table
-class Item(Base):
-    __tablename__ = "items"
+# # database table
+# class Item(Base):
+#     __tablename__ = "items"
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100), nullable=False, index=True)
-    description = Column(String(100), nullable=False, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
-    owner = relationship("User", back_populates="items")
+#     id = Column(Integer, primary_key=True, index=True)
+#     title = Column(String(100), nullable=False, index=True)
+#     description = Column(String(100), nullable=False, index=True)
+#     owner_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
+#     owner = relationship("User", back_populates="items")
