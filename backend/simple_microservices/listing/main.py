c5a52d6dde346efbb823a46b5908661b55d8ae66
allynee <86574138+allynee@users.noreply.github.com>
2023-03-13 21:01:20 +0800

    Ok basic functions confirmed

--- backend/simple_microservices/listing/main.py
+++ backend/simple_microservices/listing/main.py
@@ -9,6 +9,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from database import SessionLocal, engine
 import models
 import schemas
+import crud
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -33,47 +34,37 @@ def ping():
 
 ### GET ALL LISTINGS ###
 @app.get('/listing', response_model=List[schemas.Listing])
-async def get_all_listings(db: Session = Depends(get_db)):
-    try:
-        listing = db.query(models.Listing).all()
-        print(type(listing))
-        if not listing:
-            raise HTTPException(status_code=404, detail="Listing not found")
-        return listing
-    except SQLAlchemyError as e:
-        raise HTTPException(status_code=500, detail=e)
-    
-# @app.get('/listing', response_model=schemas.ListingBase)
-# def get_all_listings(db: Session = Depends(get_db)):
-#     listing = db.query(models.Listing).all()
-#     if not listing:
-#         raise HTTPException(status_code=404, detail="Listing not found")
-#     return listing
-    # except SQLAlchemyError as e:
-    #     raise HTTPException(status_code=500, detail=e)
+def get_all_listings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_listings(db,skip=skip,limit=limit)
+    if items is None:
+        raise HTTPException(status_code=404, detail="No listings available")
+    return items
 
 ### GET SINGLE LISTING BASED ON LISTING ID ###
 @app.get('/listing/{listing_id}', response_model=schemas.Listing)
-async def get_listing_by_id(listing_id: int, db: Session = Depends(get_db)):
-    try: 
-        listing = db.query(models.Listing).filter(models.Listing.listing_id==listing_id).first()
-        if not listing:
-            raise HTTPException(status_code=404, detail="Listing not found")
-        return vars(listing)
-    except SQLAlchemyError as e:
-        raise HTTPException(status_code=500, detail=e)
+def get_listing_by_id(listing_id: int, db: Session = Depends(get_db)):
+    listing = crud.get_listing(db, listing_id=listing_id)
+    if listing is None:
+        raise HTTPException(status_code=404, detail="Listing not found")
+    return listing
     
 ### CREATE SINGLE LISTING ###
-@app.post('/listing')
-async def create_listing(item: schemas.ListingCreate, db: Session = Depends(get_db)):
-    db_item = models.Listing(**item.dict())
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
+@app.post('/listing', response_model=schemas.Listing)
+def create_listing(listing: schemas.ListingCreate, db: Session = Depends(get_db)):
+    return crud.create_listing(db, listing=listing)
 
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=8000)
 
 # To run with debug: uvicorn main:app --reload --host 0.0.0.0 --port 8000
+
+### ASYNC DUMP ###
+# async def get_listing_by_id(listing_id: int, db: Session = Depends(get_db)):
+#     try: 
+#         listing = db.query(models.Listing).filter(models.Listing.listing_id==listing_id).first()
+#         if not listing:
+#             raise HTTPException(status_code=404, detail="Listing not found")
+#         return vars(listing)
+#     except SQLAlchemyError as e:
+#         raise HTTPException(status_code=500, detail=e)
