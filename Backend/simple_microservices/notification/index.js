18de26757c2d439be0a2c23bc61b208713bd778d
lixuennn <llx16702@gmail.com>
2023-03-14 22:51:22 +0800

    commit able to send a template email using /sendmail
    commit able to send a template email using /sendmail

--- Backend/simple_microservices/notification/index.js
+++ Backend/simple_microservices/notification/index.js
@@ -1,5 +1,7 @@
 // npm install express (to install expressjs)
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
+const mail = require('./samplemail')
 const express = require('express')
 const app = express()
 const port = 3000
@@ -10,6 +12,19 @@ app.get('/', (req, res) => {
     res.json({
         "hey": "there"
     })
+})
+
+app.get('/sendmail', (req, res) => {
+    console.log("sending mail")
+    try {
+        mail.sendMail();
+        res.status(200).send('ITS OKAY!!')
+    } catch (error) {
+        res.status(404).send('ERROR! ERROR!')
+    }
+    // res.json({
+    //     "hey": "there"
+    // })
 })
 
 app.listen(port, () => {
