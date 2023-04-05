01d11fc0990117393e5049ceac09306016478e69
allynee <86574138+allynee@users.noreply.github.com>
2023-04-05 20:44:06 +0800

    Listing update

--- backend/simple_microservices/listing/crud.py
+++ backend/simple_microservices/listing/crud.py
@@ -10,7 +10,7 @@ def get_listing(db: Session, listing_id: str):
     return db.query(models.Listing).filter(models.Listing.listing_id == listing_id).first()
 
 def get_listings(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Listing).offset(skip).limit(limit).all()
+    return db.query(models.Listing).filter(models.Listing.status == "Available").offset(skip).limit(limit).all()
 
 def create_listing(db: Session, listing: schemas.ListingCreate):
     listing = models.Listing(**listing.dict())
